c33d0aaaa67a2f1c793f46f23f8d1f8532a58bdc
AestheticHomes <admin@aesthetichomes.co.in>
2026-03-08 10:48:23 +0530

    mig

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,7 @@ module.exports = {
   outDir: 'public',
   changefreq: 'weekly',
   priority: 0.7,
+  exclude: ['/robots.txt', '/manifest.webmanifest', '/_not-found'],
   transform: async (config, path) => {
     const customPriority = path === '/' ? 1.0 : path === '/projects' || path === '/services' ? 0.9 : 0.8
     return {
